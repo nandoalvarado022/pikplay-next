7b83bc981048ddb113ff6a15070f96d4a4fc72f5
Nando (Hernando Alvarado) <nandoalvarado022@gmail.com>
2025-12-08 09:04:22 -0500

    [Feature] Evento Mallorquín (#4)
    * landing page feria mallorquin
    * varios ajustes
    * adding coupons component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,8 @@
-import styles from './button.module.scss'
+import styles from "./button.module.scss";
 
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Button as ButtonMat } from '@mui/material'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Button as MuiButton } from '@mui/material';
 
 const Button = (props) => {
   const {
@@ -22,33 +22,46 @@ const Button = (props) => {
     shine,
     realistic,
     textColor,
-  } = props
-  const className = `${classNameProp} ${animation ? styles.animation : null}`
+    width,
+    loading = false,
+  } = props;
+  const [isPressed, setIsPressed] = useState(false);
+  const className = `${classNameProp} ${animation ? styles.animation : null}`;
+
+  // return <MuiButton variant="contained" fullWidth loading={true}>{children}</MuiButton>;
 
   return (
     <motion.span
       className={`
       ButtonComponent
-      ${styles.ButtonComponent} 
-      ${realistic ? styles.realistic : ''}
-      ${shine ? styles.shine : ''}
-      ${border ? styles.border : ''}
+      ${styles.ButtonComponent}
+      ${fullWidth ? styles.fullWidth : ""}
+      ${realistic ? styles.realistic : ""}
+      ${shine ? styles.shine : ""}
+      ${border ? styles.border : ""}
       ${styles[color]}
       ${className}
-      ${outline ? styles.outline : ''}
-      ${isLink ? styles.link : ''}
-      ${fullWidth ? styles.fullWidth : ''}
-      ${disabled ? styles.disabled : ''}
+      ${outline ? styles.outline : ""}
+      ${isLink ? styles.link : ""}
+      ${fullWidth ? styles.fullWidth : ""}
+      ${disabled ? styles.disabled : ""}
+      ${isPressed ? styles.pressed : ""}
     `}
       databutton={databutton}
       id={id}
       onClick={disabled ? null : onClick}
+      onMouseDown={() => setIsPressed(true)}
+      onMouseUp={() => setIsPressed(false)}
+      onMouseLeave={() => setIsPressed(false)}
+      onTouchStart={() => setIsPressed(true)}
+      onTouchEnd={() => setIsPressed(false)}
       style={style}
       whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.9 }}>
-      <ButtonMat>{children}</ButtonMat>
+      whileTap={{ scale: 0.9 }}
+    >
+      <MuiButton loading={true}>{children}</MuiButton>
     </motion.span>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ const IndexPage = (props) => {
     const { sellersInformation, challenges } = props
     const image = ''
     const description = 'Nuestro desafío es hacer que comprar y vender sea mucho más divertido · A través de la gamificación, transformamos la experiencia de compra en algo dinámico y diferente.'
-    const title = 'Pikplay: Compra y Vende Subiendo de Nivel'
+    const title = 'Pikplay Compra y Vende Subiendo de Nivel'
     const url = ''
     return <Layout
         image={image}
@@ -18,22 +18,31 @@ const IndexPage = (props) => {
     </Layout>
 }
 
-IndexPage.getInitialProps = async (ctx) => {
+export async function getServerSideProps(ctx) {
+    return {
+        redirect: {
+            destination: '/landing-store-lead',
+            permanent: false,
+        },
+    }
+    /*
     let sellersInformation
     let challenges
     try {
         sellersInformation = await getSellerHomeSrv(ctx)
         challenges = await getChallengesByUser(ctx);
-
     } catch (error) {
         sellersInformation = []
         console.error("Error fetching sellers information:", error);
     }
 
     return {
-        sellersInformation,
-        challenges
+        props: {
+            sellersInformation,
+            challenges
+        }
     }
+    */
 }
 
 export default IndexPage;
